cd3f06ea00ab5a673942ac1e7de8106e8be0ec1a
Eileen Lowers <eileenlowers@outlook.com>
2021-09-27 16:57:26 -0400

    cheap menu setup, will enhance

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,16 @@ package com.company;
 
 //Collaborators: Zach Snyder, James Benton, Eileen Lowers
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Welcome to Figmental Bank!");
+
+        int option = 0;
+        Display display = new Display();
+//        UserAccess userAccess = new UserAccess();
+
+        Display.printHeader();
+        Display.printMainMenu();
+        display.getInput();
+
     }
+
 }
