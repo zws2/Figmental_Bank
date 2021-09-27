9a217565a0e1b4dd69a3e3472b5caeebe4f4d148
Zachary Snyder <snyderzw@hotmail.com>
2021-09-27 10:11:54 -0500

    constructors, getters/setters to account

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -4,6 +4,41 @@ public class Account {
     String name;
     int accountNumber;
     float balance;
-    
 
+    public Account(String name, int accountNumber, float balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public Account() {
+        name = "n/a";
+        accountNumber = 0;
+        balance = 0;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
 }
