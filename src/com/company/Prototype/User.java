d8b21d8c4724dc3af4227ffb6bbb7262fb13652e
James_Benton <bentonjames286@gmail.com>
2021-09-27 10:25:10 -0700

    prototype 1

---
+++ src/com/company/Prototype/User.java
@@ -0,0 +1,53 @@
+public class UserAccounts {
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    private String acctNo;
+    
+    private double balance;
+    
+    public UserAccounts(){}
+    
+    public UserAccounts(String firstName, String lastName, String acctNo, double balance){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.acctNo = acctNo;
+        this.balance = balance;
+    }
+    
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setAcctNo(String acctNo){
+        this.acctNo = acctNo;
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+    
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getAcctNo(){
+        return acctNo;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    
+    public String toString(){
+        return "User Account : [First Name=" + firstName +
+                                ", Last Name=" + lastName +
+                                ", Account No.=" + acctNo +
+                                ", Balance :=" + balance +
+                                "]";
+    }
+}
