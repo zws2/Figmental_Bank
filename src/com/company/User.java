f88122a2a76ada866a4c81c3d19a6d2428828d60
James_Benton <bentonjames286@gmail.com>
2021-09-27 14:26:53 -0700

    person deviation for Zachary

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,50 +1,35 @@
-package com.company;
-
 import java.io.Serializable;
 
 public class User implements Serializable {
-
+    
     private String firstName;
-
+    
     private String lastName;
-
-    private final int userNum;
-
+    
+    private String acctNo;
+    
     private double balance;
-
+    
     private String password;
-
-    public static int currentAccountNum = 10000;
-
-    public User(){
-        firstName = "firstName";
-        lastName = "lastName";
-        userNum = getNewAccountNum();
-        balance = 0;
-        password = "password";
-    }
-
-    public User(String firstName, String lastName, double balance, String password){
+    
+    public User(){}
+    
+    public User(String firstName, String lastName, String acctNo, double balance, String password){
         this.firstName = firstName;
         this.lastName = lastName;
-        this.userNum = getNewAccountNum();
+        this.acctNo = acctNo;
         this.balance = balance;
         this.password = password;
     }
-
-    public User(String firstName, String lastName, int userNum, double balance, String password){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userNum = userNum;
-        this.balance = balance;
-        this.password = password;
-    }
-
+    
     public void setFirstName(String firstName){
         this.firstName = firstName;
     }
     public void setLastName(String lastName){
         this.lastName = lastName;
+    }
+    public void setAcctNo(String acctNo){
+        this.acctNo = acctNo;
     }
     public void setBalance(double balance){
         this.balance = balance;
@@ -52,20 +37,15 @@ public class User implements Serializable {
     public void setPassword(String password){
         this.password = password;
     }
-
-    public static int getNewAccountNum(){
-        currentAccountNum++;
-        return currentAccountNum;
-    }
-
+    
     public String getFirstName(){
         return firstName;
     }
     public String getLastName(){
         return lastName;
     }
-    public int getUserNum(){
-        return userNum;
+    public String getAcctNo(){
+        return acctNo;
     }
     public double getBalance(){
         return balance;
@@ -73,19 +53,12 @@ public class User implements Serializable {
     public String getPassword(){
         return password;
     }
-
-    public String info(){
-        return "User Account : [First Name=" + firstName +
-                ", Last Name=" + lastName +
-                ", Account No.=" + userNum +
-                ", Balance :=" + balance +
-                "]";
-    }
-
+    
     public String toString(){
-        return firstName +
-                ", " + lastName +
-                ", " + userNum +
-                ", " + balance;
+        return "User Account : [First Name=" + firstName +
+                                ", Last Name=" + lastName +
+                                ", Account No.=" + acctNo +
+                                ", Balance :=" + balance +
+                                "]";
     }
 }
