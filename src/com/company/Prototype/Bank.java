0757c83239b297b83c4b05d43cd175562e353833
James_Benton <bentonjames286@gmail.com>
2021-09-27 10:39:37 -0700

    updates to prototype

--- src/com/company/Prototype/Bank.java
+++ src/com/company/Prototype/Bank.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class BankTransactions {
+public class Bank {
     
     private String acctNo;
     
-    private static ArrayList<UserAccounts> userAccounts;
+    private static ArrayList<User> userAccounts;
     
-    public BankTransactions(){}
+    public Bank(){}
     
     public void setAcctNo(String acctNo){
         this.acctNo = acctNo;
     }
     
-    public void setUserAccounts(ArrayList<UserAccounts> userAccounts){
+    public void setUserAccounts(ArrayList<User> userAccounts){
         this.userAccounts = userAccounts;
     }
     
@@ -22,13 +22,13 @@ public class BankTransactions {
     }
     
     //retrieve all user accounts 
-    public ArrayList<UserAccounts> getUserAccounts(){
+    public ArrayList<User> getUserAccounts(){
         return userAccounts;
     }
     
     //retreive user account by account number
-    public UserAccounts getUserByAccountNo(String acctNo){
-        for(UserAccounts u : userAccounts){
+    public User getUserByAccountNo(String acctNo){
+        for(User u : userAccounts){
             if(u.getAcctNo().equals(acctNo)){
                 return u;
             }
@@ -37,21 +37,21 @@ public class BankTransactions {
     }
     
     //update user info 
-    public void updateUserInfo(UserAccounts userAccounts){
+    public void updateUserInfo(User userAccounts){
         Iterator iterate = this.userAccounts.iterator();
         while(iterate.hasNext()){
-            UserAccounts u = (UserAccounts)iterate.next();
+            User u = (User)iterate.next();
             if(u.getAcctNo().equals(userAccounts.getAcctNo())){
-                this.userAccounts.set(this.userAccounts.get(u), userAccounts);
+                this.userAccounts.set(this.userAccounts.indexOf(u), userAccounts);
             }
         }
     }
     
     //delete user account
-    public void updateUserInfo(UserAccounts userAccounts){
+    public void deleteUserInfo(User userAccounts){
         Iterator iterate = this.userAccounts.iterator();
         while(iterate.hasNext()){
-            UserAccounts u = (UserAccounts)iterate.next();
+            User u = (User)iterate.next();
             if(u.getAcctNo().equals(userAccounts.getAcctNo())){
                 iterate.remove();
             }
