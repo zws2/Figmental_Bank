5d1ee6f72598a6af8df2d7c0499c66a8cbe27aef
Zachary Snyder <snyderzw@hotmail.com>
2021-09-27 16:24:45 -0500

    Revert "Merge branch 'Eileen' into main"
    This reverts commit ca31dca196205a5ec8e9eee42fa6d44ceed0a5f5.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,15 @@ package com.company;
 
 //Collaborators: Zach Snyder, James Benton, Eileen Lowers
 public class Main {
+
     public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Figmental Bank!");
 
-        int option = 0;
-        Display display = new Display();
-//        UserAccess userAccess = new UserAccess();
-
-        Display.printHeader();
-        Display.printMainMenu();
-        display.getInput();
-
+        Account a = new Account("Zach", 123);
+        a.deposit(100d);
+        System.out.println(a.accountDetails());
+        a.withdraw(50d);
+        System.out.println(a.accountDetails());
     }
-
 }
