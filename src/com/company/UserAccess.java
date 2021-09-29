b845dd2c5413fd5ec5fda3258eccc8c1102b90dd
Eileen Lowers <eileenlowers@outlook.com>
2021-09-29 18:27:42 -0400

    update user registration method

--- src/com/company/UserAccess.java
+++ src/com/company/UserAccess.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 public class UserAccess {
@@ -13,7 +15,58 @@ public class UserAccess {
     }
 
     public static void registerUser(Scanner scan) {
+        Random random = new Random();
 
+        String firstName;
+        String lastName;
+        String acctNo = "1";
+        double deposit = 0;
+        double balance = 0;
+        String username;
+        String password;
+
+        ArrayList<String> accountSet = new ArrayList<>();
+
+        System.out.println("Thank you for creating an account with us.\n");
+        System.out.println("Please enter your first name:");
+        firstName = scan.nextLine();
+        System.out.println("Please enter your last name:");
+        lastName = scan.nextLine();
+
+        for (int i = 0; i < 2; i++)
+        {
+            int n = random.nextInt(10);
+            acctNo += Integer.toString(n);
+        }
+
+        System.out.println("Thank you, " + firstName + "!\n" +
+                "To open an account, we require a minimum balance of $5.\n" +
+                "How much would you like to deposit to open your account?");
+        deposit = scan.nextDouble();
+        if(!(deposit >= 5)) {
+            System.out.println("The amount you entered does not meet the minimum.");
+        }
+        else {
+            System.out.println("The amount you entered is $" + deposit + ".");
+            balance = balance + deposit;
+        }
+
+        accountSet.add(firstName);
+        accountSet.add(lastName);
+        accountSet.add(acctNo);
+        accountSet.add(String.valueOf(balance));
+
+        System.out.println("Thank you, " + firstName + lastName + "!\n" +
+                "Your account has been created!\n" +
+                "Your account number is " + acctNo + "and your new balance is " + balance + ".\n" +
+                "To access your account, please enter a username and secure password.\n");
+
+        System.out.println("Username: ");
+        username = scan.nextLine();
+        System.out.println("Password: ");
+        password = scan.nextLine();
+
+        loginInfo.put(username, password);
 
     }
 
