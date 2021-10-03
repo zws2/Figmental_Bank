f3453509b1cf5917597eea41af5b4ce68e4e8bcd
Eileen Lowers <eileenlowers@outlook.com>
2021-10-03 13:27:35 -0400

    saving some comments for later

--- src/com/company/Display.java
+++ src/com/company/Display.java
@@ -1,11 +1,17 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Display {
 
     static Scanner scan = new Scanner(System.in);
     static int option = 0;
+
+    public static void startMenu() {
+        printHeader();
+        printUserMenu();
+    }
 
     static void printHeader() {
         System.out.println("*********************************************");
@@ -15,29 +21,30 @@ public class Display {
     }
 
     //prints out a fancy menu of choices
-    static void printMainMenu() {
+    static void printUserMenu() {
         System.out.println("*********************************************");
         System.out.println("Please choose one of the following options:");
         System.out.println("1) Existing Account Login");
         System.out.println("2) Account Registration");
         System.out.println("3) Exit");
         System.out.println("*********************************************");
-        getInput(option);
-        userMenu(option);
+        userMenu();
     }
 
-    static void printAccountMenu() {
+    private static void printAccountMenu(User currentUser) {
         System.out.println("*********************************************");
         System.out.println("Please choose one of the following options:");
         System.out.println("1) Create New Account");
         System.out.println("2) View Accounts");
         System.out.println("3) Exit");
         System.out.println("*********************************************");
-        getInput(option);
-        manageAccountMenu(option);
+        manageAccountMenu(currentUser);
     }
 
-    static void printManageAccountMenu() {
+    private static void selectAccountMenu(User currentUser) {
+    }
+
+    private static void printManageAccountMenu() {
         System.out.println("*********************************************");
         System.out.println("Please choose one of the following options:");
         System.out.println("1) Check Balance");
@@ -47,12 +54,13 @@ public class Display {
         System.out.println("5) View Transactions");
         System.out.println("6) Exit");
         System.out.println("*********************************************");
-        getInput(option);
-        viewAccountMenu(option);
+        viewAccountMenu();
     }
 
+
+
     //initializes option to zero, takes user input for Menus
-    static int getInput(int option) {
+    static int getInput() {
         do {
             try {
                 option = Integer.parseInt(scan.nextLine());
@@ -67,14 +75,19 @@ public class Display {
         return option;
     }
 
-    static void userMenu(int option) {
-        switch (option) {
+    static void userMenu() {
+        switch (getInput()) {
             case 1:
-                //option 1 - login existing account user
-//                UserAccess.loginUser();
+                //option 1 - login existing user
+                User currentUser = UserAccess.loginUser();
+                if(currentUser != null) {
+                    printAccountMenu(currentUser);
+                } else {
+                    System.out.println("Invalid login!");
+                }
                 break;
             case 2:
-                //option 2 - register new account
+                //option 2 - register new user
                 UserAccess.registerUser();
                 break;
             case 3:
@@ -85,20 +98,22 @@ public class Display {
             default:
                 //any other input results in error and returns to beginning of the method
                 System.out.println("Whoops! Something done broke!");
-                userMenu(option);
+                userMenu();
         }
+
     }
 
-    static void manageAccountMenu(int option) {
-        switch (option) {
+    static void manageAccountMenu(User currentUser) {
+        switch (getInput()) {
             case 1:
                 //create new account
-                //add to bank hashmap - HashMap<Integer, Account> accounts
-                //add to userlist - addAccountNum?
+                createUserAcct(currentUser);
+                printAccountMenu(currentUser);
                 break;
             case 2:
-                //select account / get account list method
-                printAccountList();
+                //get account list method
+                printAccountList(currentUser);
+                selectAccountMenu(currentUser);
                 break;
             case 3:
                 //option 3 - exit
@@ -108,31 +123,54 @@ public class Display {
             default:
                 //any other input results in error and returns to beginning of the method
                 System.out.println("Whoops! Something done broke!");
-                manageAccountMenu(option);
+                manageAccountMenu(currentUser);
         }
     }
 
-    static void viewAccountMenu(int option) {
-        switch (option) {
+    private static void createUserAcct(User currentUser) {
+        Account userAcct = new Account(currentUser.getUserName());
+        Bank.putAccount(userAcct);
+        Bank.writeAccounts();
+        currentUser.addAccountNum(userAcct.getAccountNum());
+        Bank.putUser(currentUser);
+        Bank.writeUsers();
+    }
+
+
+    private static void printAccountList(User currentUser) {
+        ArrayList<Account> accountList = currentUser.getAccounts();
+        if (accountList.size() == 0) {
+            System.out.println("No accounts are registered to this user.");
+            printAccountMenu(currentUser);
+        }
+        else {
+            for(int i = 1; i < accountList.size() + 1; i++) {
+               System.out.println(i + ") " + accountList.get(i - 1));
+            }
+        }
+    }
+
+    static void viewAccountMenu() {
+        switch (getInput()) {
             case 1:
                 //balance
-                printBalance();
+//                printBalance();
                 break;
             case 2:
                 //deposit
-                inputDeposit();
+//                inputDeposit();
                 break;
             case 3:
                 //withdraw
-                inputWithdraw();
+//                inputWithdraw();
                 break;
             case 4:
                 //transfer
-                inputTransfer();
+//                inputTransfer();
                 break;
             case 5:
                 //transactions print
-                printTransaction();
+//                printTransaction();
                 break;
             case 6:
                 //exit
@@ -142,22 +180,25 @@ public class Display {
             default:
                 //any other input results in error and returns to beginning of the method
                 System.out.println("Whoops! Something done broke!");
-                viewAccountMenu(option);
+                viewAccountMenu();
         }
     }
 
-    private static void printBalance() {
-    }
-    private static void inputDeposit() {
-    }
-    private static void inputWithdraw() {
-    }
-    private static void inputTransfer() {
-    }
-    private static void printTransaction() {
-    }
-    private static void printAccountList() {
-        //getAccountNumbers?
-    }
+
+
+//    private static void printBalance() {
+//    }
+//    private static void inputDeposit() {
+//    }
+//    private static void inputWithdraw() {
+//    }
+//    private static void inputTransfer() {
+//    }
+//    private static void printTransaction() {
+//        Bank.getTransactionArrayList();
+//    }
+//    private static void printAccountList() {
+//        //getAccountNumbers?
+//    }
 
 }
